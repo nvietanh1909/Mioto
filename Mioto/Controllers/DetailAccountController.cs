563b2b678fd81104ca3cfe7b46205e1c494d29e2
nvietanh1909 <nvietanh.190904@example.com>
2024-06-14 19:18:42 +0700

    Add project files.

---
+++ Mioto/Controllers/DetailAccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mioto.Controllers
+{
+    public class DetailAccountController : Controller
+    {
+        // GET: DetailAccount
+        public ActionResult InfoAccount()
+        {
+            return View();
+        }
+        public ActionResult FavoriteCar()
+        {
+            return View();
+        }
+        public ActionResult Gift()
+        {
+            return View();
+        }
+        public ActionResult MyCar()
+        {
+            return View();
+        }
+        public ActionResult MyTrip()
+        {
+            return View();
+        }
+
+        public ActionResult LongTermOrder()
+        {
+            return View();
+        }
+        public ActionResult RequestDeleteAccount()
+        {
+            return View();
+        }
+        public ActionResult MyAddress()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mioto/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mioto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult IntroOwnerCar()
+        {
+            return View();
+        }
+        public ActionResult RegisterOwner()
+        {
+            return View();
+        }
+    }
+}
